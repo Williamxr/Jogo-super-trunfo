e986ba0e9e9e7a4a6cf7d271f077e0cb490043b5
William <william.teletabs@hotmail.com>
2022-03-21 15:48:42 -0300

    Criando objetos com atributos

---
+++ js.js
@@ -0,0 +1,32 @@
+var carta1 = {
+  nome: "Bulbasaur",
+  atributos: {
+    ataque: 7,
+    defesa: 6,
+    magia: 8, 
+  }
+};
+
+var carta2 = {
+  nome: "Squirtle",
+  atributos: {
+    ataque: 6,
+    defesa: 8,
+    magia: 7, 
+  }
+};
+
+var carta3 = {
+  nome: "Charmander",
+  atributos: {
+    ataque: 8,
+    defesa: 6,
+    magia: 7, 
+  }
+};
+
+var cartas = [carta1, carta2, carta3];
+var cartaBot = 0
+var cartaJogador = 0
+
+console.log(cartas);
